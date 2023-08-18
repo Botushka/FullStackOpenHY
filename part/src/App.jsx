b268913a2f4b53bbeedf444efaa9cfa6366d6f1b
BuildTools <91798274+Botushka@users.noreply.github.com>
2023-08-18 18:28:51 +0300

    osa2

--- part/src/App.jsx
+++ part/src/App.jsx
@@ -1,53 +1,102 @@
 import { useState } from 'react'
 
-const Header = (props) => {
-  console.log(props)
-    return(
-    <h1>{props.name}</h1>
-    )
-  }
-  
-  const Content = (props) => {
-    console.log(props)
-    return(
-      <p>{props.name} {props.exercises}</p>
-    )
-  }
-  
-  const Total = (props) => {
-    console.log(props)
-    return(
-      <p>Number of exercises {props.total}</p>
-    )
-  }
-  
-  const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-
+const History = ({ good, bad, neutral }) => {
+  if (good === 0 && bad === 0 && neutral === 0) {
     return (
       <div>
-        <Header name={course} />
-        <Content name={parts[0].name} exercises={parts[0].exercises} />
-        <Content name={parts[1].name} exercises={parts[1].exercises} />
-        <Content name={parts[2].name} exercises={parts[2].exercises} />
-        <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+        <h1>Statistics</h1>
+        No feedback yet
       </div>
-    )
+    );
   }
-  
+
+  return (
+    <div>
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <div>
+    {text} {value}
+  </div>
+);
+
+const Statistics = ({ good, bad, neutral }) => {
+  const all = neutral + good + bad;
+  const avg = (good - bad) / all;
+  const pos = (good / all) * 100;
+
+  const cellStyle = {
+    padding: '8px',
+    textAlign: 'right'
+  };
+
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td style={cellStyle}><StatisticLine text="good" value={good} /></td>
+          </tr>
+          <tr>
+            <td style={cellStyle}><StatisticLine text="neutral" value={neutral} /></td>
+          </tr>
+          <tr>
+            <td style={cellStyle}><StatisticLine text="bad" value={bad} /></td>
+          </tr>
+          <tr>
+            <td style={cellStyle}><StatisticLine text="all" value={all} /></td>
+          </tr>
+          <tr>
+            <td style={cellStyle}><StatisticLine text="average" value={avg.toFixed(2)} /></td>
+          </tr>
+          <tr>
+            <td style={cellStyle}><StatisticLine text="positive" value={pos.toFixed(2) + '%'} /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const App = () => {
+  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+ 
+
+  const handleFeedbackClick = (feedbackType) => {
+    if (feedbackType === 'bad') {
+      setBad(bad + 1);
+    } else if (feedbackType === 'good') {
+      setGood(good + 1);
+    } else if (feedbackType === 'neutral') {
+      setNeutral(neutral + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <div>
+        <Button handleClick={() => handleFeedbackClick('bad')} text='bad' />
+        <Button handleClick={() => handleFeedbackClick('good')} text='good' />
+        <Button handleClick={() => handleFeedbackClick('neutral')} text='neutral' />
+      </div>
+      <History good={good} bad={bad} neutral={neutral} />
+    </div>
+  )
+}
+
   export default App
   
